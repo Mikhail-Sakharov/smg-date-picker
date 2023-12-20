69a3f9060ff98f86df13f58d4540bd24acf107e0
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-12-20 19:38:04 +0300

    feat: adds header builder

---
+++ src/view-builders/header.builder.ts
@@ -0,0 +1,25 @@
+import {HeaderData} from '../header-data.interface';
+import {Lang} from '../localization';
+import {Localization} from '../localization.enum';
+
+export const buildHeader = (localization: Localization, headerData: HeaderData) => {
+  const {currentMonth, currentYear} = headerData;
+
+  const calendarHeaderElement = document.createElement('div');
+  calendarHeaderElement.classList.add('smg-date-picker__header');
+  const calendarLeftArrowElement = document.createElement('div');
+  calendarLeftArrowElement.classList.add('smg-date-picker__left-arrow');
+  const calendarTitleElement = document.createElement('div');
+  calendarTitleElement.classList.add('smg-date-picker__title');
+  calendarTitleElement.textContent = `${Object.values(Lang[localization].months)[currentMonth]} ${currentYear}`;
+  calendarTitleElement.setAttribute('data-month', currentMonth.toString());
+  calendarTitleElement.setAttribute('data-year', currentYear.toString());
+  const calendarRightArrowElement = document.createElement('div');
+  calendarRightArrowElement.classList.add('smg-date-picker__right-arrow');
+
+  calendarHeaderElement.append(calendarLeftArrowElement);
+  calendarHeaderElement.append(calendarTitleElement);
+  calendarHeaderElement.append(calendarRightArrowElement);
+
+  return calendarHeaderElement;
+};
