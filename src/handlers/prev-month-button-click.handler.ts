5552cbcdfb226494e0fcb5bc2bb8b59a4a2a5e7f
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-12-20 19:46:47 +0300

    feat: adds prev month button click handler

---
+++ src/handlers/prev-month-button-click.handler.ts
@@ -0,0 +1,49 @@
+import {createCalendarData} from '../create-calendar-data';
+import {Lang} from '../localization';
+import {Localization} from '../localization.enum';
+import {buildCalendarDays} from '../view-builders/calendar-days.builder';
+
+export const handlePrevMonthButtonClick = (
+  anchorElement: HTMLElement,
+  headerElement: HTMLElement,
+  datePickerContainerElement: HTMLElement,
+  // mode: CalendarMode,
+  localization: Localization,
+  restoreDayElementClickHandler: (collection: NodeListOf<Element>) => void
+) => {
+  const headerTitleElement = headerElement.querySelector('.smg-date-picker__title');
+  const month = Number(headerTitleElement?.getAttribute('data-month'));
+  const prevMonth = month === 0 ? 11 : month - 1;
+  const year = Number(headerTitleElement?.getAttribute('data-year'));
+  const prevYear = month === 0 ? year - 1 : year;
+
+  if (headerTitleElement) {
+    headerTitleElement?.setAttribute('data-month', `${prevMonth}`);
+    headerTitleElement?.setAttribute('data-year', `${prevYear}`);
+    headerTitleElement.textContent = `${Object.values(Lang[localization].months)[prevMonth]} ${prevYear}`;
+  }
+
+  const updatedCalendarData = createCalendarData(prevMonth, prevYear);
+
+  const rangeOptions = {
+    anchorElementDateValue: anchorElement.getAttribute('data-date'),
+    anchorElementStartDateValue: anchorElement.getAttribute('data-start-date'),
+    anchorElementFinishDateValue: anchorElement.getAttribute('data-finish-date')
+  };
+
+  const updatedCalendarDaysElementView = buildCalendarDays(updatedCalendarData, rangeOptions);
+  const updatedCalendarDayCollection = updatedCalendarDaysElementView.querySelectorAll('.smg-date-picker__day');
+  restoreDayElementClickHandler(updatedCalendarDayCollection);
+
+  // switch(mode) {
+  //   case CalendarMode.Single:
+  //     addDayElementClickHandlers(updatedCalendarDayCollection);
+  //     break;
+  //   case CalendarMode.Range:
+  //     addDayElementClickHandlersRangeMode(updatedCalendarDayCollection);
+  //     break;
+  // }
+
+  const deprecatedCalendarDaysElementView = datePickerContainerElement.querySelector('.smg-date-picker__days');
+  deprecatedCalendarDaysElementView?.replaceWith(updatedCalendarDaysElementView);
+};
