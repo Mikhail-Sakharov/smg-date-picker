277103ced1c83d30492404dbb1fbbe5d1bf5e233
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-12-20 19:39:09 +0300

    feat: adds weekday names builder

---
+++ src/view-builders/weekday-names.builder.ts
@@ -0,0 +1,18 @@
+import {Lang} from '../localization';
+import {Localization} from '../localization.enum';
+
+export const buildWeekdayNames = (localization: Localization) => {
+  const calendarWeekdayNamesElement = document.createElement('div');
+  calendarWeekdayNamesElement.classList.add('smg-date-picker__weekday-names');
+  Object.values(Lang[localization].weekdays).forEach((weekDay) => {
+    const weekdayElement = document.createElement('div');
+    const weekdaySpanElement = document.createElement('span');
+    weekdayElement.classList.add('smg-date-picker__weekday');
+    weekdaySpanElement.classList.add('smg-date-picker__weekday-name');
+    weekdaySpanElement.textContent = weekDay;
+    weekdayElement.append(weekdaySpanElement);
+    calendarWeekdayNamesElement.append(weekdayElement);
+  });
+
+  return calendarWeekdayNamesElement;
+};
