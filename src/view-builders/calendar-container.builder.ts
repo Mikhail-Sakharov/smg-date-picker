fe0104c6e4b44dffd8f8159a0cec512a9cd4e0f7
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-12-20 19:41:13 +0300

    feat: adds calendar container builder

---
+++ src/view-builders/calendar-container.builder.ts
@@ -0,0 +1,6 @@
+export const buildCalendarContainer = () => {
+  const calendarContainerElement = document.createElement('div');
+  calendarContainerElement.classList.add('smg-date-picker__container');
+
+  return calendarContainerElement;
+};
