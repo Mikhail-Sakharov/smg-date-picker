bd6298ce4e743be15d920d4cf12947ae51d65871
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-12-25 21:54:35 +0300

    feat: adds options object for SMGDatePicker

--- src/smg-date-picker.ts
+++ src/smg-date-picker.ts
@@ -1,26 +1,41 @@
 import {CalendarPresenter} from './calendar-presenter';
 import {CalendarMode} from './enums/calendar-mode.enum';
 import {Localization} from './enums/localization/localization.enum';
+import {SMGDatePickerInterface} from './interfaces/smg-date-picker.interface';
 
 export class SMGDatePicker {
-  constructor(
-    private anchorElement: HTMLElement | null = null,
-    private firstOutputElement: HTMLElement | null = null,
-    private secondOutputElement: HTMLElement | null = null,
-    private mode = CalendarMode.Single,
-    private localization = Localization.Eng,
-    private callback: (startDate: string, finishDate?: string) => void | null
-  ) {}
+  private anchorElement: HTMLElement | null = null;
+  private firstOutputElement: HTMLElement | null = null;
+  private secondOutputElement: HTMLElement | null = null;
+  private mode = CalendarMode.Single;
+  private localization = Localization.Eng;
+  private callback: ((startDate: string, finishDate?: string) => void) | null = null;
+
+  constructor({
+    anchorElement,
+    firstOutputElement,
+    secondOutputElement,
+    mode,
+    localization,
+    callback
+  }: SMGDatePickerInterface) {
+    this.anchorElement = anchorElement;
+    this.firstOutputElement = firstOutputElement;
+    this.secondOutputElement = secondOutputElement;
+    this.mode = mode;
+    this.localization = localization;
+    this.callback = callback;
+  }
 
   public create = () => {
     const datePicker = new CalendarPresenter(
-    this.anchorElement,
-    this.firstOutputElement,
-    this.secondOutputElement,
-    this.mode,
-    this.localization,
-    this.callback
-  );
-  datePicker.create();
+      this.anchorElement,
+      this.firstOutputElement,
+      this.secondOutputElement,
+      this.mode,
+      this.localization,
+      this.callback
+    );
+    datePicker.create();
   };
 }
